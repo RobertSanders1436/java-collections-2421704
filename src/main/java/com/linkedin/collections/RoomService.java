2de70e344b1a763ec8851cb276fa4f35fdabce38
Robert Sanders <robert.sanders@csuglobal.edu>
2021-11-23 02:29:56 -0800

    first try

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -1,14 +1,16 @@
 package com.linkedin.collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class RoomService {
 
-	// 1. Declare a Collection to store Room Inventory
+	Collection<Room> inventory;
 
 	public RoomService() {
-		
-	// 2. Initialize Collection and assign it to the Room Inventory
+		this.inventory = new ArrayList<>();
+
 
 	}
 
@@ -16,24 +18,29 @@ public class RoomService {
 		
 	// 3. Return the Room Inventory
 		
-		return null;
+		return new ArrayList<>(this.inventory);
 	}
 	
 	public void createRoom(String name, String type, int capacity, double rate) {
 	
 	// 4. Add a new Room to the Room Inventory using the provided parameters
+		this.inventory.add(new Room(name, type, capacity, rate));
 
 	}
 
 	public void createRooms(Room[] rooms) {
 	
 	// 5. Add the Rooms provided in the Array to the Room Inventory
+		this.inventory.addAll(Arrays.asList(rooms));
+
 
 	}
 	
 	public void removeRoom(Room room) {
 		
 	// 6. Remove the provided Room from the Room Inventory
+		this.inventory.remove(room);
+
 
 	}
 
